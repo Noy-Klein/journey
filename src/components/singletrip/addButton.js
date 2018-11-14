399afee87ffd00e5b4127b5ae9518eb21cbfee44
unknown <yuvalmoshe345@gmail.com>
2018-11-14 09:48:01 +0200

    addForm

--- src/components/singletrip/addButton.js
+++ src/components/singletrip/addButton.js
@@ -5,5 +5,51 @@ import { observable, action } from "mobx";
 @inject("store")
 @observer
 class AddButton extends Component {
+    @observable trip = {
+        title: "",
+        description: "",
+        startDate: "",
+        endDate: "",
+        people: "",
+        adress: "",
+        pictures: ""
+    };
 
+    closeButton = () => {
+        this.props.closePopup(null)
+    }
+
+    @action inputChange = (t) => {
+        this.trip[t.target.name] = t.target.value;
+      }
+
+      
+
+    addTrip = () => { 
+        this.props.store.addTrip(this.trip.title, this.trip.description, this.trip.startDate, this.trip.endDate, this.trip.people, this.trip.adress, this.trip.pictures)
+    }
+
+    render() {
+        return (
+            <div className="add-button">
+                <div className="popUp">
+                <input className="inputTitle" type="text" name="title" placeholder="title" value={this.title} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputDescription" type="text" name="description" placeholder="description" value={this.description} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputStartDate" type="text" name="startDate" placeholder="startDate" value={this.startDate} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputEndDate" type="text" name="endDate" placeholder="endDate" value={this.endDate} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputPeople" type="text" name="people" placeholder="people" value={this.people} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputAdress" type="text" name="adress" placeholder="adress" value={this.adress} onChange={this.inputChange}/>
+                <br></br><br></br>
+                <input className="inputPictures" type="text" name="pictures" placeholder="pictures" value={this.pictures} onChange={this.inputChange}/>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default AddButton;
--- src/store/tripStore.js
+++ src/store/tripStore.js
@@ -1,8 +1,30 @@
-import { observable} from "mobx";
+import { observable, action} from "mobx";
 
 class TripStore {
     @observable
-    trip = ""
+    trip = null;
+}
+
+getTrip = async () =>{
+    let trip = await axios.get('http://localhost:1000/trip/')
+    this.setTrip(trip.data)
+}
+
+@action setTrip = (trip) => {
+    this.trip = trip;
+}
+
+addTrip = async (title, description, startDate,endDate, people, adress, pictures) => {
+    let trip = await axios.post('http://localhost:1000/trip',{
+        title: title,
+        description: description,
+        startDate: startDate,
+        endDate: endDate,
+        people: people,
+        adress: adress,
+        pictures: pictures
+    })
+    this.setTrip(trip)
 }
 
 const store = new TripStore();
