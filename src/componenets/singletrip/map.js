f432fc4c50160fa97ccd10effb5f98f73488a989
nuzzi123 <nuzzi123@gmail.com>
2018-11-14 10:08:28 +0200

    'try-to-merge'

--- src/componenets/singletrip/map.js
+++ src/componenets/singletrip/map.js
@@ -1,13 +1,12 @@
 import { Map, InfoWindow, Marker, GoogleApiWrapper, Polyline, Polygon } from 'google-maps-react';
 import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
 
-@inject('store')
-@observer
 export class MapContainer extends Component {
     onMarkerClick = () => {
         alert('here')
     }
+
+
     render() {
 
         const triangleCoords = [
@@ -16,20 +15,36 @@ export class MapContainer extends Component {
             { lat: 32.321, lng: -64.757 },
             { lat: 25.774, lng: -80.190 }
         ];
+        //[2.320582, 48.859489]
+        //[48.859489, 2.320582]
+        // const center = {lat: 48.859489, lng: 2.320582}
+        // const center = [48.859489, 2.320582]
+        // console.log(this.props.google)
         return (
-            <Map style={{ width: '50%', height: '50%' }} initialCenter={'the first coordinant'} google={this.props.google} zoom={14}>
+            <Map style={{ width: '50%', height: '50%' }} initialCenter={{ lat: 48.859489, lng: 2.320582 }} google={this.props.google} zoom={14}>
 
                 <Polygon
-                    paths={'all of the coordinants - axios request and populate'}
+                    paths={
+                        [
+                            { lat: 43.70724, lng: 7.2617893 },
+                            { lat: 48.859489, lng: 2.320582 }
+                        ]
+                    }
                     strokeColor="#0000FF"
                     strokeOpacity={0.8}
                     strokeWeight={2} />
-                {/* map through the checkpoint and for each add a marker with name and position, and the paths get only the coordinants */}
+
                 <Marker onClick={this.onMarkerClick}
-                    name={'checkpoint name'} />
+                    name={'Current location'} />
                 <Marker onClick={this.onMarkerClick}
-                    position={'checkpoint coordinants'}
+                    position={{ lat: 43.70724, lng: 7.2617893 }}
                     name={'p'} />
+
+                {/* <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>Israel</h1>
+                    </div>
+                </InfoWindow> */}
             </Map>
         );
     }
--- src/store/tripStore.js
+++ src/store/tripStore.js
@@ -1,16 +1,49 @@
-import { observable } from "mobx";
-import Axios from 'axios';
+import { observable, action } from "mobx";
 
 class TripStore {
-    @observable trip = null
+    @observable trip = null;
+    @observable showpopupaddtrip = false;
 
-    @action addCheckPoint = async (newCheckPoint) => {
-        let data = await Axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + newCheckPoint.adress + '&key=AIzaSyA-NDun_On5Bx3TerMVbAaC8jfU7jotv8M')
-        newCheckPoint.coordinants = data.result[0].geometry.location;
-        this.trip.checkPoints.push(newCheckPoint)
+    getTrip = async () => {
+        let trip = await axios.get('http://localhost:1000/trip/')
+        this.setTrip(trip.data)
     }
 
+    @action setTrip = (trip) => {
+        this.trip = trip;
+    }
+
+
+    @action changeshowpopupaddtrip = () => {
+        if (this.showpopupaddtrip) {
+            this.showpopupaddtrip = false;
+        }
+        else {
+            this.showpopupaddtrip = true;
+        }
+    }
+    addTrip = async (title, description, startDate, endDate, people, adress, pictures) => {
+        let trip = await axios.post('http://localhost:1000/trip', {
+            title: title,
+            description: description,
+            startDate: startDate,
+            endDate: endDate,
+            people: people,
+            adress: adress,
+            pictures: pictures
+        })
+        this.setTrip(trip)
+
+    }
 }
 
 const store = new TripStore();
+
 export default store;
+
+
+// @action addCheckPoint = async (newCheckPoint) => {
+//         let data = await Axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + newCheckPoint.adress + '&key=AIzaSyA-NDun_On5Bx3TerMVbAaC8jfU7jotv8M')
+//         newCheckPoint.coordinants = data.result[0].geometry.location;
+//         this.trip.checkPoints.push(newCheckPoint)
+//     }
