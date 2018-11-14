7eba0d0965670b736ec5fe2243495e55989cecf3
nuzzi123 <nuzzi123@gmail.com>
2018-11-14 18:26:03 +0200

    'fixed-map!'

--- Apis/mapApi.js
+++ Apis/mapApi.js
@@ -3,21 +3,30 @@ const router = express.Router();
 const trips = require('../models/tripModel');
 const checkpoint = require('../models/checkpointModel');
 
-router.get('/checkpoints', (req,res)=>{
-    checkpoint.find({}).exec((err, data)=>{
-        if(err){
+router.get('/checkpoints', (req, res) => {
+    checkpoint.find({}).exec((err, data) => {
+        if (err) {
             res.status(500).send(err)
         }
-        else{
-           res.send(data) 
+        else {
+            res.send(data)
         }
     })
 });
 
-router.post('/checkpoints', (req,res)=>{
-    let newCheckPoint = new checkpoint(req.body);
-    newCheckPoint.save();
-    res.send(newCheckPoint);
+router.post('/checkpoints', (req, res) => {
+    let id = req.body.object.id
+    let newCheckPoint = new checkpoint(req.body.object.data);
+    trips.findOneAndUpdate({ _id: id }, {$push: {checkpoints: newCheckPoint}}).exec((err, trip) => {
+        newCheckPoint.coordinant = req.body.coo;
+        newCheckPoint.save((err, newCheckPoint) => {
+            trip.checkpoints.push(newCheckPoint);
+            trip.save((err, trip) => {
+                console.log(trip)
+                res.send(trip.coordinant);
+            });
+        });
+    })
 })
 
 module.exports = router
--- src/store/tripStore.js
+++ src/store/tripStore.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 class TripStore {
 
     @observable trip = null;
-    @observable trips = [this.getTrips()]
+    @observable trips = [];
     @observable showpopupaddtrip = false;
 
     @action getTrips = async () => {
@@ -17,33 +17,31 @@ class TripStore {
     }
 
     @action setTrip = async (id) => {
-        // let tripsDemo = await axios.get('http://localhost:1000/trips')
         await this.getTrips();
-        // tripsDemo = tripsDemo.data
         let theTrip = this.trips.find(t => t._id === id)
         this.trip = theTrip
     }
 
     @action changeshowpopupaddtrip = () => {
-        this.showpopupaddtrip= !this.showpopupaddtrip
+        this.showpopupaddtrip = !this.showpopupaddtrip
     }
 
     Addtrip = async (trip) => {
-        let newtrip = await axios.post('http://localhost:1000/trips', {trip})
+        let newtrip = await axios.post('http://localhost:1000/trips', { trip })
         this.setTrip(newtrip)
     }
 
-    @action addCheckPoint = async (newCheckPoint) =>{ //send the trips id
-        let data = await axios.post('https://maps.googleapis.com/maps/api/geocode/json?address=' + newCheckPoint.adress + '&key=AIzaSyA-NDun_On5Bx3TerMVbAaC8jfU7jotv8M')
-        let checkpoint = await axios.post('http://localhost:1000/checkpoints', newCheckPoint);
-        // console.log(data.data.results[0].geometry.location)
-        // console.log(checkpoint.data.checkpoint)
-        checkpoint.data.coordinant = data.data.results[0].geometry.location; //returns a bigger object whith key data?
-        this.trip.checkpoints.push(checkpoint.data._id)
-        // console.log(this.trip)
-        // let coorCP = await axios.get('http://localhost:1000/checkpoints');
-        // let cp = coorCP.data.find(c => c._id === checkpoint.data._id);
-        // console.log(cp.coordinant)
+    @action addCheckPoint = async (newCheckPoint) => {
+
+        try {
+            let data = await axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + newCheckPoint.data.adress + '&key=AIzaSyA-NDun_On5Bx3TerMVbAaC8jfU7jotv8M')
+            let checkpoint = await axios.post('http://localhost:1000/checkpoints', { object: newCheckPoint, coo: data.data.results[0].geometry.location });
+            this.trips = checkpoint.data
+            console.log(this.trip);
+        }
+        catch (err) {
+            console.error(err)
+        }
     }
 }
 
