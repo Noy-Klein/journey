d815e0ad7859b3646360814cae251abb20302d30
unknown <yuvalmoshe345@gmail.com>
2018-11-21 14:09:29 +0200

    hadas

--- src/components/users/landing.js
+++ src/components/users/landing.js
@@ -7,7 +7,7 @@ class Landing extends Component {
     render() {
         return (
             <div>
-                <h5>Welcome to MY JOURNEY</h5>
+                <h5>Welcome Welcome to MY JOURNEY</h5>
                 <br />
                 <h6>How many times did you get to fly abroad and after a few years you were upset that you did not record every minute of your journey?</h6>
                 <h6>On your journey after army? On a trip to Poland? Roots trip? It probably happened a few times...</h6>
